9c23d2d0ce6fe066ddc7439f514ca5aa2a7efeb0
afusco059 <70164839+afusco059@users.noreply.github.com>
2021-09-30 10:11:44 -0400

    Add files via upload

---
+++ P(3)Logic and Conditionals/P(3)Logic and Conditionals/P(3)Logic and Conditionals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_3_Logic_and_Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool beach = true;
+            bool hiking = false;
+            bool city = true;
+
+            bool yourNeeds = (beach && city);
+            bool friendNeeds = (beach || hiking);
+
+            bool tripDecision = (yourNeeds && friendNeeds);
+            Console.WriteLine(tripDecision);
+
+        }
+        static void Conditionals()
+        {
+            double pepperLength = 4;
+
+            string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
+
+            Console.WriteLine(message);
+        }
+    }
+}
